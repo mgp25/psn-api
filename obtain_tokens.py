414a0ca3a28dddd3588509ee9a37786e9535147e
mgp25 <me@mgp25.com>
2018-05-03 02:32:40 +0200

    Update obtain_tokens.py
    I forgot to update this script on github. This includes a fix to write the dict properly to a file

--- obtain_tokens.py
+++ obtain_tokens.py
@@ -2,13 +2,14 @@ from psnapi.Auth import Auth
 from psnapi.Friend import Friend
 from psnapi.User import User
 from psnapi.Messaging import Messaging
+import json
 
 auth = Auth('YOUR_EMAIL', 'YOUR_PASSWORD', 'TICKET_UUID', '2FA')
 
 tokens = auth.get_tokens()
 
 f = open('tokens', 'wt', encoding='utf-8')
-f.write(s)
+f.write(json.dumps(tokens))
 f.close()
 
 print('Your tokens have been saved in the file \'tokens\'')
